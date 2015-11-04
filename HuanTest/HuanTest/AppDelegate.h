3e6ec1cca386f35aaef7023165642c9ee933544d
navy1994 <lihaijun1994@gmail.com>
2015-11-04 15:55:42 +0800

    创建了HuanTest

---
+++ HuanTest/HuanTest/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  HuanTest
+//
+//  Created by mac- t4 on 15/11/4.
+//  Copyright (c) 2015年 上海科文麦格里实业有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ HuanTest/HuanTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HuanTest
+//
+//  Created by mac- t4 on 15/11/4.
+//  Copyright (c) 2015年 上海科文麦格里实业有限公司. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
